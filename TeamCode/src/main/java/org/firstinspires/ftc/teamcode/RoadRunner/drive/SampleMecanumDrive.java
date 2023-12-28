462d950038bb481e5c28c63dfca8e534ead478dc
BetheBob1 <BetheBob082208@gmail.com>
2023-12-27 16:08:28 -0800

    RoadRunnerTuningMeet2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/SampleMecanumDrive.java
@@ -55,7 +55,7 @@ public class SampleMecanumDrive extends MecanumDrive {
     public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(9, 0, 1.5);
     public static PIDCoefficients HEADING_PID = new PIDCoefficients(8, 0, 1);
 
-    public static double LATERAL_MULTIPLIER = 1.0106;
+    public static double LATERAL_MULTIPLIER = 1.01265822785;
 
     public static double VX_WEIGHT = 1;
     public static double VY_WEIGHT = 1;
