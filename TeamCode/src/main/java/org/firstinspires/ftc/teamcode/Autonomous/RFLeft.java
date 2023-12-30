c35f494ac047cb19492ef4c84a878bd6ad4accb0
Jesse He <heqiqi2014@hotmail.com>
2023-12-30 12:45:52 -0800

    auto stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RFLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RFLeft.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import android.util.Log;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.trajectory.constraints.AngularVelocityConstraint;
 import com.acmerobotics.roadrunner.trajectory.constraints.MinVelocityConstraint;
@@ -9,19 +11,93 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.RoadRunner.util.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.RoadRunner.util.trajectorysequence.TrajectorySequenceBuilder;
 import org.firstinspires.ftc.teamcode.Variables.Detection;
 import org.firstinspires.ftc.teamcode.Variables.VisionProcessors;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 
 @Config
-@Autonomous(name = "BlueRedLeft(actual)", group = "Linear OpMode")
+@Autonomous(name = "RFLeft", group = "Linear OpMode")
 public class RFLeft extends MeepMeepBoilerplate{
+
+    TrajectorySequence generate_sequence(BufferedReader reader, SampleMecanumDrive drive, Servo passiveServo, Servo autoServo){
+
+        TrajectorySequenceBuilder sequence = drive.trajectorySequenceBuilder(getCurrentPosition(drive));
+        for (String line: reader.lines().toArray(String[]::new)) {
+
+            telemetry.addLine("Iterating! "+line);
+            telemetry.update();
+            if (line.indexOf("//")>=0){
+                telemetry.addLine("Found comment: "+line);
+                continue;
+            }
+
+            String[] elements = line.split(":");
+            assert elements.length==2;
+            if(elements.length<=1){
+                continue;
+            }
+            String command = elements[0];
+            String parameter = elements[1];
+            String bs = ("Stuff: " + command + " more stuff: " + parameter +" one last thing: " + line);
+            telemetry.addLine(bs);
+            Log.d("RFLeft", bs);
+            switch (command){
+                case "back"->{
+                    double param = Double.parseDouble(parameter);
+                    sequence = sequence.back(param);
+                }
+                case "waitSeconds"->{
+                    double param = Double.parseDouble(parameter);
+                    sequence = sequence.waitSeconds(param);
+                }
+                case "strafeRight"->{
+                    double param = Double.parseDouble(parameter);
+                    sequence = sequence.strafeRight(param);
+                }
+                case "strafeLeft"->{
+                    double param = Double.parseDouble(parameter);
+                    sequence = sequence.strafeLeft(param);
+                }
+                case "forward"->{
+                    double param = Double.parseDouble(parameter);
+                    sequence = sequence.forward(param);
+                }
+                case "turn"->{
+                    double param = Double.parseDouble(parameter);
+                    sequence = sequence.turn(Math.toRadians(param));
+                }
+                case "passiveServoSetPosition"->{
+                    double param = Double.parseDouble(parameter);
+                    sequence = sequence.addTemporalMarker(() -> passiveServo.setPosition(param));
+                }
+                case "autoServoSetPosition"->{
+                    double param = Double.parseDouble(parameter);
+                    sequence = sequence.addTemporalMarker(() -> autoServo.setPosition(param));
+                }
+                default -> {
+                    telemetry.addLine("Defaulted!!!!!!!: "+ command);
+                    telemetry.update();
+                }
+            }
+
+        }
+        return sequence.build();
+    }
+
+
     @Override
     public void runOpMode() {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         Servo passiveServo = hardwareMap.get(Servo.class, "passiveServo");
         Servo autoServo = hardwareMap.get(Servo.class, "autoServo");
+        autoServo.setPosition(0.78);
         initVision(VisionProcessors.TFOD);
         Detection detection = Detection.UNKNOWN;
         TrajectoryVelocityConstraint slowConstraint = new MinVelocityConstraint(Arrays.asList(
@@ -38,100 +114,64 @@ public class RFLeft extends MeepMeepBoilerplate{
         }
         autoServo.setPosition(0.35);
         switch (detection) {
-            case LEFT -> drive.followTrajectorySequence(
-                    drive.trajectorySequenceBuilder(getCurrentPosition(drive))
-                            .forward(28.0)
+            case LEFT ->{
+
+                BufferedReader reader;
+                try {
+                    reader = new BufferedReader(new FileReader("/sdcard/FIRST/training/RFLeftLeft.txt"));
+                    drive.followTrajectorySequence(generate_sequence(reader,drive, passiveServo, autoServo));
+                } catch (FileNotFoundException e) {
+                    drive.followTrajectorySequence(
+                            drive.trajectorySequenceBuilder(getCurrentPosition(drive))
+                            .back(28.0)
                             .turn(Math.toRadians(90))
-                            .forward(3)
-                            .waitSeconds(1)
-                            .addTemporalMarker(() -> passiveServo.setPosition(0.1))
-                            .waitSeconds(1)
                             .back(6)
-                            .strafeRight(25)
-                            .waitSeconds(.5)
-                            .back(78)
-                            .waitSeconds(.25)
-                            .turn(Math.toRadians(180))
-                            .waitSeconds(.25)
-                            .strafeRight(15)
-                            .waitSeconds(.25)
-                            .forward(18)
-                            .waitSeconds(.25)
-                            .addTemporalMarker(() -> autoServo.setPosition(0.68))
-                            .waitSeconds(1.5)
-                            .addTemporalMarker(() -> autoServo.setPosition(0.6))
-                            .waitSeconds(.5)
-                            .addTemporalMarker(() -> autoServo.setPosition(0.35))
-                            .waitSeconds(.5)
-                            .back(4)
-                            .waitSeconds(.25)
-                            .strafeLeft(20)
-                            .waitSeconds(.25)
-                            .forward(16)
-                            .build()
-            );
-            case CENTER -> { drive.followTrajectorySequence(
-                    drive.trajectorySequenceBuilder(getCurrentPosition(drive))
-                            .forward(31.5)
-                            .waitSeconds(.25)
-                            .addDisplacementMarker(() -> passiveServo.setPosition(0.1))
-                            .waitSeconds(.25)
-                            .forward(23)
-                            .turn(Math.toRadians(-90))
-                            .waitSeconds(.5)
-                            .back(78)
-                            .waitSeconds(.25)
-                            .turn(Math.toRadians(180))
-                            .waitSeconds(.25)
-                            .strafeRight(21)
-                            .waitSeconds(.25)
-                            .forward(18)
-                            .waitSeconds(.25)
-                            .addTemporalMarker(() -> autoServo.setPosition(0.68))
-                            .waitSeconds(1.5)
-                            .addTemporalMarker(() -> autoServo.setPosition(0.6))
-                            .waitSeconds(.5)
-                            .addTemporalMarker(() -> autoServo.setPosition(0.35))
-                            .waitSeconds(.5)
-                            .back(4)
-                            .waitSeconds(.25)
-                            .strafeLeft(26)
-                            .waitSeconds(.25)
-                            .forward(16)
-                            .build());
-            }
-            case RIGHT -> drive.followTrajectorySequence(
-                    drive.trajectorySequenceBuilder(getCurrentPosition(drive))
-                            .forward(28.0)
-                            .turn(Math.toRadians(-90))
-                            .forward(6)
                             .waitSeconds(1)
                             .addTemporalMarker(() -> passiveServo.setPosition(0.1))
                             .waitSeconds(1)
-                            .back(5)
-//                            .strafeLeft(22)
-//                            .waitSeconds(.5)
-//                            .forward(18)
-//                            .setVelConstraint(slowConstraint)
-//                            .waitSeconds(.25)
-//                            .forward(20)
-//                            .resetVelConstraint()
-//                            .forward(43)
-//                            .waitSeconds(.25)
-//                            .turn(Math.toRadians(-90))
-//                            .waitSeconds(.25)
-//                            .forward(21.5)
-//                            .waitSeconds(.25)
-//                            .strafeLeft(4)
-//                            .addTemporalMarker(() -> autoServo.setPosition(0.68))
-//                            .waitSeconds(1.5)
-//                            .addTemporalMarker(() -> autoServo.setPosition(0.6))
-//                            .waitSeconds(.5)
-//                            .addTemporalMarker(() -> autoServo.setPosition(0.35))
-//                            .waitSeconds(.5)
-//                            .strafeRight(2)
+                            .forward(4)
                             .build()
             );
+                }
+      }
+            case CENTER -> {
+
+                BufferedReader reader;
+                try {
+                    reader = new BufferedReader(new FileReader("/sdcard/FIRST/training/RFLeftCenter.txt"));
+                    drive.followTrajectorySequence(generate_sequence(reader,drive, passiveServo, autoServo));
+                } catch (FileNotFoundException e) {
+                    drive.followTrajectorySequence(
+                            drive.trajectorySequenceBuilder(getCurrentPosition(drive))
+                            .back(31.0)
+                            .waitSeconds(1)
+                            .addDisplacementMarker(() -> passiveServo.setPosition(0.1))
+                            .waitSeconds(1)
+                            .forward(4)
+                            .build()
+            );
+                }
+            }
+            case RIGHT -> {
+
+                BufferedReader reader;
+                try {
+                    reader = new BufferedReader(new FileReader("/sdcard/FIRST/training/RFLeftRight.txt"));
+                    drive.followTrajectorySequence(generate_sequence(reader,drive, passiveServo, autoServo));
+                } catch (FileNotFoundException e) {
+                    drive.followTrajectorySequence(
+                            drive.trajectorySequenceBuilder(getCurrentPosition(drive))
+                            .back(28.0)
+                            .turn(Math.toRadians(-90))
+                            .back(3.5)
+                            .waitSeconds(1)
+                            .addTemporalMarker(() -> passiveServo.setPosition(0.2))
+                            .waitSeconds(1)
+                            .forward(5)
+                            .build()
+            );
+                }
+      }
             default -> {
                 telemetry.addLine("Warning: Cup not detected");
                 telemetry.update();
