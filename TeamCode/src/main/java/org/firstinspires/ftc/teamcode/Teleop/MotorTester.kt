6e31dd7d1ee0cab227a099e467a614bc446fb629
BetheBob1 <BetheBob082208@gmail.com>
2024-01-23 21:33:57 -0800

    all4motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/MotorTester.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/MotorTester.kt
@@ -4,6 +4,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
 import org.firstinspires.ftc.teamcode.DriveMethods
+import org.firstinspires.ftc.teamcode.Variables.motorBL
+import org.firstinspires.ftc.teamcode.Variables.motorBR
+import org.firstinspires.ftc.teamcode.Variables.motorFL
+import org.firstinspires.ftc.teamcode.Variables.motorFR
 import org.firstinspires.ftc.teamcode.Variables.slideRotationMotor
 
 @Disabled
@@ -12,16 +16,42 @@ class MotorTester: DriveMethods() {
     override fun runOpMode() {
         initMotorsSecondBot()
         initSlideMotors()
-        val motorBeingTested: DcMotor = slideRotationMotor!!
+        val motorBeingTested1: DcMotor = motorBR!!
 
-        motorBeingTested.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
-        motorBeingTested.mode = DcMotor.RunMode.RUN_USING_ENCODER
+        motorBeingTested1.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        motorBeingTested1.mode = DcMotor.RunMode.RUN_USING_ENCODER
+
+        val motorBeingTested2: DcMotor = motorBL!!
+
+        motorBeingTested2.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        motorBeingTested2.mode = DcMotor.RunMode.RUN_USING_ENCODER
+
+        val motorBeingTested3: DcMotor = motorFR!!
+
+        motorBeingTested3.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        motorBeingTested3.mode = DcMotor.RunMode.RUN_USING_ENCODER
+
+        val motorBeingTested4: DcMotor = motorFL!!
+
+        motorBeingTested4.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        motorBeingTested4.mode = DcMotor.RunMode.RUN_USING_ENCODER
         waitForStart()
 
         while (opModeIsActive()) {
-            motorBeingTested.power = gamepad2.left_stick_y.toDouble() / 4
+            if (gamepad1.a){
+                motorBeingTested1.power = 1.0;
+            }
+            if (gamepad1.b){
+                motorBeingTested2.power = 1.0;
+            }
+            if (gamepad1.x){
+                motorBeingTested3.power = 1.0;
+            }
+            if (gamepad1.y){
+                motorBeingTested4.power = 1.0;
+            }
 
-            telemetry.addData("Motor Value: ", motorBeingTested.currentPosition)
+            telemetry.addData("Motor Value1: ", motorBeingTested1.currentPosition)
             telemetry.update()
         }
     }
