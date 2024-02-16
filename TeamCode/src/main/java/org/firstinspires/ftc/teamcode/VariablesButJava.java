46534d221dc0f9a2d4a0e43e1792afc584995dfb
BetheBob1 <BetheBob082208@gmail.com>
2024-02-15 17:38:08 -0800

    rfisf

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablesButJava.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver.BlinkinPattern;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+public class VariablesButJava {
+    public static double servoRestPosition = 0.245;
+    public static double servoMidPosition = 0.32;
+    public static double servoPlacePosition = 0.65;
+}
